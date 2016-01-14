086c081cb7dd0a4bf847837dcfa928f609b08f00
Zach Alewel <zachalewel@gmail.com>
2016-01-14 09:09:27 -0700

    Adding User table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224031019) do
+ActiveRecord::Schema.define(version: 20160105174238) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20151224031019) do
     t.text     "picture"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
