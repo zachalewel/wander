6f81089dd86e5e61b3e6932a6099f095004c320d
Zach Alewel <zachalewel@gmail.com>
2016-02-01 21:42:06 -0700

    Adds User partial for login and signup

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    username { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    password_confirmation { password }
+  end
+end
