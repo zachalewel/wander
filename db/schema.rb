92516904d0e304fcb48d20129a0f51b04fbd8c73
Zach Alewel <zachalewel@gmail.com>
2016-10-06 13:13:35 -0600

    trip version 1.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105174238) do
+ActiveRecord::Schema.define(version: 20161006184239) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,26 @@ ActiveRecord::Schema.define(version: 20160105174238) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.text     "trip_notes"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "trips_users", id: false, force: :cascade do |t|
+    t.integer "trip_id", null: false
+    t.integer "user_id", null: false
+  end
+
+  add_index "trips_users", ["trip_id", "user_id"], name: "index_trips_users_on_trip_id_and_user_id"
+  add_index "trips_users", ["user_id", "trip_id"], name: "index_trips_users_on_user_id_and_trip_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
